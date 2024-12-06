22473ead13ecd044ec75e3a2c3f8f3f293957205
MrGalva07 <lucas.galvao0707@gmail.com>
2024-12-06 00:44:02 -0300

    changesToBuild2

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,16 +1,16 @@
-'use client'
+'use client';
 
-import React from 'react'
-import LoginForm from '../../components/Forms/LoginForm'
-
+import React, { Suspense } from 'react';
+import LoginForm from '../../components/Forms/LoginForm';
 
 const Page = () => {
-    return (
-        <div className='items-center flex bottom-0  bg-gradient-login '>
-            <LoginForm />
-          
-        </div>
-      )
-}
+  return (
+    <div className="items-center flex bottom-0 bg-gradient-login">
+      <Suspense fallback={<div>Loading...</div>}>
+        <LoginForm />
+      </Suspense>
+    </div>
+  );
+};
 
-export default Page
+export default Page;
